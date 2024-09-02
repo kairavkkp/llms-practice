6d310c77bd74437cc356502bfd0d24c39e997755
kairavkkp <kairavpithadia13@gmail.com>
2024-09-02 11:29:35 +0530

    feat: bpe tokenizers

--- 2_data/data.py
+++ 2_data/data.py
@@ -1,4 +1,5 @@
 import re
+import tiktoken
 from importlib.metadata import version
 from simple_tokenizer_v1 import SimpleTokenizerV1
 
@@ -31,3 +32,9 @@ ids = tokenizer.encode(text=sample_text)
 print("DEBUG: Encode: ", ids)
 original_text = tokenizer.decode(ids=ids)
 print("DEBUG: Decode: ", original_text)
+
+# BytePair Encoding
+tokenizer = tiktoken.get_encoding("gpt2")
+text = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace."
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print("DEBUG: Integers from tiktoken: ", integers)
